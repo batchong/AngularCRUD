991fb3aa0bc07e22e9c50b71cb7b7e18ea98fd62
Julian Ernest Camello <julianernestcamello@gmail.com>
2020-09-22 16:56:52 +0800

    Added routing

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,9 +1,25 @@
+import { ViewProductComponent } from './view-product/view-product.component';
+import { ViewAllProductsByDateComponent } from './view-all-products-by-date/view-all-products-by-date.component';
+import { ViewAllProductsByCategoryComponent } from './view-all-products-by-category/view-all-products-by-category.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { ListAllProductsComponent } from './list-all-products/list-all-products.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
+import { AddProductComponent } from './add-product/add-product.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProductsComponent } from './products.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'add-product', component: AddProductComponent },
+  { path: 'delete-product', component: DeleteProductComponent },
+  { path: 'list-all-product', component: ListAllProductsComponent },
+  { path: 'update-product', component: UpdateProductComponent },
+  { path: 'view-all-products-by-category', component: ViewAllProductsByCategoryComponent },
+  { path: 'view-all-products-by-date', component: ViewAllProductsByDateComponent },
+  { path: 'view-product', component: ViewProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,10 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { ListAllProductsComponent } from './list-all-products/list-all-products.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
+import { ViewAllProductsByDateComponent } from './view-all-products-by-date/view-all-products-by-date.component';
+import { ViewAllProductsByCategoryComponent } from './view-all-products-by-category/view-all-products-by-category.component';
 
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [
+    ProductsComponent, 
+    AddProductComponent, 
+    ViewProductComponent, 
+    ListAllProductsComponent, 
+    UpdateProductComponent, 
+    DeleteProductComponent, 
+    ViewAllProductsByDateComponent, 
+    ViewAllProductsByCategoryComponent
+  ],
   imports: [
     CommonModule,
     ProductsRoutingModule
